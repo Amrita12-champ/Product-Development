db8baf43f1f344b94ef73c3009fca9b892025ba4
pravat <pravatpanigrahi1234@gmail.com>
2026-09-05 00:43:15 +0530

    feat: add farmer product development backend

---
+++ product-development/src/test/java/com/framer/product_development/ProductDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framer.product_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
